1b5a37f4d4bd1dd3e9d42d1750cc9aff52c08ea1
François Benaiteau <francois.benaiteau@gmail.com>
2018-01-22 18:03:41 +0100

    Change select to radio buttons to respect the spec.

--- app/forms/decidim/participations/admin/participation_form.rb
+++ app/forms/decidim/participations/admin/participation_form.rb
@@ -15,7 +15,7 @@ module Decidim
         attribute :justification, String
         attribute :moderation, ModerationForm
         validates :participation_type, :moderation, presence: true
-        validates :recipient_role, presence: true, if: ->(form) { form.participation_type.present? }
+        validates :recipient_role, presence: true, if: ->(form) { form.participation_type == "question" }
 
         validates :category, presence: true, if: ->(form) { form.category_id.present? }
         delegate :categories, to: :current_feature
