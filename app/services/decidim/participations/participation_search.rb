5c33fe0f4896422d0475eb573b55e5bd836e4687
Harry Gueguen <harry.gueguen@gmail.com>
2018-01-23 11:10:54 +0100

    Filters for state and type

--- app/services/decidim/participations/participation_search.rb
+++ app/services/decidim/participations/participation_search.rb
@@ -32,6 +32,19 @@ module Decidim
         end
       end
 
+      # Handle the type filter
+      def search_participation_type
+        if participation_type == "questions"
+          query.where(participation_type: "Question")
+        elsif participation_type == "contributions"
+          query.where(participation_type: "Contribution")
+        elsif participation_type == "opinions"
+          query.where(participation_type: "Opinion")
+        else # Assume 'all'
+          query
+        end
+      end
+
       # Handle the activity filter
       def search_activity
         if activity.include? "voted"
@@ -50,10 +63,8 @@ module Decidim
         case state
         when "accepted"
           query.accepted
-        when "rejected"
-          query.rejected
-        when "evaluating"
-          query.evaluating
+        when "waiting_answers"
+          query.where(state: nil)
         else # Assume 'all'
           query
         end
