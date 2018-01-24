badbc6cba40234c22721f4c71b950abd3fb24812
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-24 14:39:45 +0100

    Update answer + some trads

--- app/commands/decidim/participations/admin/answer_participation.rb
+++ app/commands/decidim/participations/admin/answer_participation.rb
@@ -24,6 +24,8 @@ module Decidim
           return broadcast(:invalid) if form.invalid?
 
           answer_participation
+          update_answer_state
+          update_moderation
           broadcast(:ok)
         end
 
@@ -35,8 +37,19 @@ module Decidim
           participation.update_attributes!(
             state: @form.state,
             answer: @form.answer,
-            answered_at: Time.current
+            answered_at: Time.current,
+            #justification
           )
+        end
+
+        def update_answer_state
+          if participation.answer_state == "waiting_for_answer" && participation.answer.present?
+            participation.update_attributes(answer_state: "waiting_for_validation")
+          end
+        end
+
+        def update_moderation
+          participation.moderation.update_attributes(upstream_moderation: "authorized") if participation.state == "accepted"
         end
       end
     end
