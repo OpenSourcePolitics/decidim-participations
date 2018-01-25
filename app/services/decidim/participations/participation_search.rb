0551601d03113b5622ed14c3abf5c07e072e793a
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-25 14:10:55 +0100

    Index filter WIP

--- app/services/decidim/participations/participation_search.rb
+++ app/services/decidim/participations/participation_search.rb
@@ -62,7 +62,7 @@ module Decidim
       def search_state
         case state
         when "waiting_for_answer"
-          redirect_to decidim_participatory_process_participations_path(participatory_process: query.first.feature.participatory_space, filter: {state: "waiting_for_answer"})
+          # redirect_to decidim_participatory_process_participations_path(participatory_process: query.first.feature.participatory_space, filter: {state: "waiting_for_answer"})
           query.where(participation_type: "question").joins(:moderation).merge(Moderation.where(['upstream_moderation = ?', "waiting_for_answer"]))
         when "published_answer"
           query.where(participation_type: "question").joins(:moderation).merge(Moderation.where(['upstream_moderation = ?', "authorized"]))
