559f5df67eb061ed8129fdd8d99d6641d1d43b28
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-02-23 15:18:15 +0000

    Edit access to process (back office)

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -1,5 +1,6 @@
 begin
     require 'decidim/monkey_patches/mailer/notification_mailer_patches.rb'
+    require 'decidim/monkey_patches/models/participatory_process_patches.rb'
   rescue LoadError
     puts "Monkey patches were not loaded"
   end
